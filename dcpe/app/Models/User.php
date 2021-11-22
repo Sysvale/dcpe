5b8e88016e5580a05f346cf387a1905c8152a430
Geidson Benicio Coelho <geidsonc@gmail.com>
2021-11-22 18:18:24 -0300

    Problema na conexão com o mongodb

--- dcpe/app/Models/User.php
+++ dcpe/app/Models/User.php
@@ -2,15 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
