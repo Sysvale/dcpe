e0473d5ff05f5f595cbbf240c54a52f5c560c890
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2021-12-06 23:21:50 -0300

    adiciona os componentes basicos do menu e views do laravel

---
+++ resources/js/core/common/HeaderTitle.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+        <div class="title"><h2>{{ title }}</h2></div>
+        <span class="subtitle">{{ subtitle }}</span>
+        <hr/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: String,
+        subtitle: String,
+    },
+
+    data() {
+        return {
+
+        };
+    },
+};
+
+</script>
+
+<style scoped>
+
+.header {
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.subtitle {
+    font-size: 1rem;
+}
+</style>
